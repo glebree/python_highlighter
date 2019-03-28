f2b60b0dde2211018747724f6cd11b84cfa326d0
Gleb Bryksin <gb@Glebs-MacBook-Pro.local>
2019-03-28 21:45:24 +0200

    Highlighter implementation with two separate functions

--- highlighter/__init__.py
+++ highlighter/__init__.py
@@ -21,44 +21,49 @@ def create_app():
     def process():
         search_text = request.form['search']
         text = request.form['text']
-        highlighted_text = highlight_text(text, search_text)
+        marked_text = markup_text(text, search_text)
         result = {'text': text,
-                  'highlighted_text': Markup(highlighted_text),
+                  'highlighted_text': Markup(marked_text),
                   }
         return render_template(template_file_name, **result)
 
-    def highlight_text(text, search_text):
+    def highlight_text(text, expr):
         """Markup searched string in given text.
         @:param text - string text to be processed
         @:return marked text, e.g., "sample text <mark>highlighted part</mark> rest of the text"."""
-        lower_text, search_text = text.lower(), search_text.lower()
+        lower_text, expr = text.lower(), expr.lower()
         highlighted_text = []
-        open_tag = '<mark>'
-        close_tag = '</mark>'
 
         search = 0
         while search < len(lower_text):
-            search = lower_text.find(search_text, search, len(lower_text))
-            if search != -1:
+            search = lower_text.find(expr, search, len(lower_text))
+            if not search == -1:
                 result = highlighted_text.append(search)
-                search = lower_text.find(search_text, search + 1, len(lower_text))
+                search = lower_text.find(expr, search + 1, len(lower_text))
             else:
                 break
+        return highlighted_text
 
-        result = highlighted_text
+    def markup_text(text, search_text):
+        """Markup given text.
+        @:param text - string text to be marked
+        @:return marked text, e.g., <mark>highlighted text</mark>."""
+        result = highlight_text(text, search_text)
+        open_tag = '<mark>'
+        close_tag = '</mark>'
+
         if len(result) > 0:
             result_1 = result
             result_1 = [i + len(search_text) for i in result_1]
-
             index = len(result) - 1
             new_text = text
             while index >= 0:
-                marked_text = new_text[:result[index]] + \
-                          open_tag + new_text[result[index]:result_1[index]] \
-                          + close_tag + new_text[result_1[index]:]
+                final_text = new_text[:result[index]] + \
+                             open_tag + new_text[result[index]:result_1[index]] \
+                             + close_tag + new_text[result_1[index]:]
                 index -= 1
-                new_text = marked_text
-            return marked_text
+                new_text = final_text
+            return final_text
         else:
             return text
 
